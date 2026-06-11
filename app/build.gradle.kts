3c02c475428345574cb1c232eea58860373f03e7
Kevinectv <34521425+kevinectv@users.noreply.github.com>
2026-06-11 16:05:54 -0500

    build: bump version to 2.0.1 and add install permission
    Increment application version and add REQUEST_INSTALL_PACKAGES permission to support in-app updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
     applicationId = "com.aistudio.luminaplay.iptv.vpxwqr"
     minSdk = 21
     targetSdk = 34
-    versionCode = 2
-    versionName = "2.0.0"
+    versionCode = 3
+    versionName = "2.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
